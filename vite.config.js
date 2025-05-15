2735698c7754fdf6445676d6024f6340d84ef459
Observatorio <rotpp.planeacion@boyaca.gov.co>
2025-05-15 10:39:00 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 8080,
+    host: true
+  },
+  preview: {
+    port: 8080,
+    host: true
+  }
 })
